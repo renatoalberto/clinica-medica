882c3ff7bbf7201f9d79f19a715b24a99f25206b
Renato Alberto <renato.alb@gmail.com>
2019-03-19 09:36:16 -0300

    icones e página responsiva

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var express = require('express');
-var app = express();
+var favicon = require('serve-favicon');
+var path    = require('path');
+var app     = express();
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3000;
